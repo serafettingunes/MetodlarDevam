c2e50d732821daee2bea69255ec10918c1ae9b48
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-21 19:24:30 +0300

    Add project files.

---
+++ MetodlarDevam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetodlarDevam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
